9afdbcc87537b0fd5f044f0e7376ca622366b9bb
souha sekma <77791869+souhasekma@users.noreply.github.com>
2022-04-13 11:30:46 +0100

    Update panier.js

--- src/components/panier/panier.js
+++ src/components/panier/panier.js
@@ -1,9 +1,9 @@
 import  React from 'react';
-import { Button} from 'react-bootstrap';
+
 import {useNavigate} from "react-router-dom";
 import { Link } from 'react-router-dom';
 function Panier (authorized){
-    let navigate = useNavigate();
+   
     return (
         <div>
         <h2>Vos achats</h2> 
